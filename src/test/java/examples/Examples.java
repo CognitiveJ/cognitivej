005deb45193a47cec1d2edce68637dbd6d39c5c7
Sam <sam@developersam.com>
2018-04-25 16:28:18 -0400

    Added partial example for findSimilar

--- src/test/java/examples/Examples.java
+++ src/test/java/examples/Examples.java
@@ -3,6 +3,9 @@ package examples;
 import cognitivej.vision.computervision.ComputerVisionScenario;
 import cognitivej.vision.computervision.ImageDescription;
 import cognitivej.vision.computervision.OCRResult;
+import cognitivej.vision.face.facelist.FaceList;
+import cognitivej.vision.face.persongroup.PersistedFace;
+import cognitivej.vision.face.persongroup.PersonGroup;
 import cognitivej.vision.face.scenario.FaceScenarios;
 import cognitivej.vision.face.scenario.ImageHolder;
 import cognitivej.vision.face.scenario.ImageNamingStrategy;
@@ -29,31 +32,66 @@ public final class Examples {
      */
     private static final String IMAGE_URL = "DUMMY";
     /**
-     * Dummy candidates image URL.
+     * Some images of famous people to test grouping.
      */
-    private static final String CANDIDATE_1 = "C1", CANDIDATE_2 = "C2";
+    private static final String OBAMA_1 = "http://thehill.com/sites/default/files/styles/" +
+            "thumb_small_article/public/blogs/obama2_0.jpg",
+            OBAMA_2 = "https://upload.wikimedia.org/wikipedia/commons/8/8d/" +
+                    "President_Barack_Obama.jpg",
+            GABEN_1 = "https://ih1.redbubble.net/image.464960440.1473/" +
+                    "raf,750x1000,075,t,f89f2b:1617e563f0.u5.jpg",
+            GABEN_2 = "https://i.gadgets360cdn.com/large/Gabe_Newell_1484763796244.jpg",
+            GABEN_3 = "https://files.gamebanana.com/img/ss/srends/530-90_5129d4a3be73b.jpg",
+            JEFF_DEAN_1 = "https://thumbor.forbes.com/thumbor/960x0/" +
+                    "https%3A%2F%2Fblogs-images.forbes.com%2Fpeterhigh%2Ffiles%2F2016%2F08%2F" +
+                    "Jeff-Dean-3-1200x566.gif",
+            JEFF_DEAN_2 = "https://media.wired.com/photos/5ad69141e7f4053764abb7fc/" +
+                    "master/pass/Jeff-Dean-FINAL.jpg";
+    
+    /**
+     * Obtain the face scenarios.
+     *
+     * @return the constructed face scenarios.
+     */
+    private static FaceScenarios faceScenarios() {
+        return new FaceScenarios(
+                getProperty("azure.cognitive.face.subscriptionKey"),
+                getProperty("azure.cognitive.emotion.subscriptionKey"));
+    }
     
     /**
      * Face detection.
      */
-    private static void faceDetect() {
-        FaceScenarios faceScenarios = new FaceScenarios(
-                getProperty("azure.cognitive.face.subscriptionKey"),
-                getProperty("azure.cognitive.emotion.subscriptionKey"));
+    public static void faceDetect() {
         ImageOverlayBuilder imageOverlayBuilder = ImageOverlayBuilder.builder(IMAGE_URL);
         imageOverlayBuilder.outlineFacesOnImage(
-                faceScenarios.findFaces(IMAGE_URL),
+                faceScenarios().findFaces(IMAGE_URL),
                 RectangleType.FULL, CognitiveJColourPalette.STRAWBERRY).launchViewer();
+    }
+    
+    public static void faceSimilar() {
+        String adHocFaceListID = "ad-hoc";
+        FaceList faceList = faceScenarios()
+                .faceList()
+                .createFaceList(adHocFaceListID, "Ad Hoc", null)
+                .withResult();
+        PersistedFace persistedFace = faceScenarios().faceList()
+                .addFaceToFaceList(adHocFaceListID, null, null, OBAMA_1).then()
+                .addFaceToFaceList(adHocFaceListID, OBAMA_2).then()
+                .addFaceToFaceList(adHocFaceListID, GABEN_1).then()
+                .addFaceToFaceList(adHocFaceListID, GABEN_2).then()
+                .addFaceToFaceList(adHocFaceListID, GABEN_3).then()
+                .addFaceToFaceList(adHocFaceListID, JEFF_DEAN_1).then()
+                .addFaceToFaceList(adHocFaceListID, JEFF_DEAN_2)
+                .withResult();
+        // TODO
     }
     
     /**
      * Face landmarks detection.
      */
-    private static void faceLandmarks() {
-        FaceScenarios faceScenarios = new FaceScenarios(
-                getProperty("azure.cognitive.face.subscriptionKey"),
-                getProperty("azure.cognitive.emotion.subscriptionKey"));
-        Face faces = faceScenarios.findSingleFace(IMAGE_URL);
+    public static void faceLandmarks() {
+        Face faces = faceScenarios().findSingleFace(IMAGE_URL);
         ImageOverlayBuilder.builder(IMAGE_URL)
                 .outFaceLandmarksOnImage(faces).launchViewer();
     }
@@ -61,11 +99,8 @@ public final class Examples {
     /**
      * Face attribute detection.
      */
-    private static void faceAttribute() {
-        FaceScenarios faceScenarios = new FaceScenarios(
-                getProperty("azure.cognitive.face.subscriptionKey"),
-                getProperty("azure.cognitive.emotion.subscriptionKey"));
-        List<Face> faces = faceScenarios.findFaces(IMAGE_URL);
+    public static void faceAttribute() {
+        List<Face> faces = faceScenarios().findFaces(IMAGE_URL);
         ImageOverlayBuilder.builder(IMAGE_URL)
                 .outlineFacesOnImage(faces, RectangleType.CORNERED, CognitiveJColourPalette.MEADOW)
                 .writeFaceAttributes(faces, CognitiveJColourPalette.MEADOW).launchViewer();
@@ -74,58 +109,47 @@ public final class Examples {
     /**
      * Verify faces.
      */
-    private static void faceVerify() {
-        FaceScenarios faceScenarios = new FaceScenarios(
-                getProperty("azure.cognitive.face.subscriptionKey"),
-                getProperty("azure.cognitive.emotion.subscriptionKey"));
-        ImageOverlayBuilder imageOverlayBuilder = ImageOverlayBuilder.builder(CANDIDATE_1);
-        imageOverlayBuilder.verify(CANDIDATE_2, faceScenarios.verifyFaces(CANDIDATE_1, CANDIDATE_2))
+    public static void faceVerify() {
+        ImageOverlayBuilder imageOverlayBuilder = ImageOverlayBuilder.builder(OBAMA_1);
+        imageOverlayBuilder.verify(OBAMA_2,
+                faceScenarios().verifyFaces(OBAMA_1, OBAMA_2))
                 .launchViewer();
     }
     
     /**
      * Identify faces.
      */
-    private static void faceIdentify() {
-        FaceScenarios faceScenarios = new FaceScenarios(
-                getProperty("azure.cognitive.face.subscriptionKey"),
-                getProperty("azure.cognitive.emotion.subscriptionKey"));
+    public static void faceIdentify() {
         ImageOverlayBuilder imageOverlayBuilder = ImageOverlayBuilder.builder(IMAGE_URL);
         List<ImageHolder> candidates = new ArrayList<>();
         People people = ScenarioHelper.createPeopleFromHoldingImages(
                 candidates, ImageNamingStrategy.DEFAULT);
-        String groupId = faceScenarios.createGroupWithPeople(
+        String groupId = faceScenarios().createGroupWithPeople(
                 randomAlphabetic(6).toLowerCase(), people);
     }
     
     /**
      * Pixelate images.
      */
-    private static void facePixelate() {
-        FaceScenarios faceScenarios = new FaceScenarios(
-                getProperty("azure.cognitive.face.subscriptionKey"),
-                getProperty("azure.cognitive.emotion.subscriptionKey"));
+    public static void facePixelate() {
         ImageOverlayBuilder imageOverlayBuilder =
                 ImageOverlayBuilder.builder(IMAGE_URL);
-        faceScenarios.findFaces(IMAGE_URL).forEach(imageOverlayBuilder::pixelateFaceOnImage);
+        faceScenarios().findFaces(IMAGE_URL).forEach(imageOverlayBuilder::pixelateFaceOnImage);
         imageOverlayBuilder.launchViewer();
     }
     
     /**
      * Detect emotions.
      */
-    private static void detectEmotions() {
-        FaceScenarios faceScenarios = new FaceScenarios(
-                getProperty("azure.cognitive.face.subscriptionKey"),
-                getProperty("azure.cognitive.emotion.subscriptionKey"));
+    public static void detectEmotions() {
         ImageOverlayBuilder.builder(IMAGE_URL).outlineEmotionsOnImage(
-                faceScenarios.findEmotionFaces(IMAGE_URL)).launchViewer();
+                faceScenarios().findEmotionFaces(IMAGE_URL)).launchViewer();
     }
     
     /**
      * Find a description of the image.
      */
-    private static void describeImage() {
+    public static void describeImage() {
         ComputerVisionScenario computerVisionScenario = new ComputerVisionScenario(
                 getProperty("azure.cognitive.vision.subscriptionKey"));
         ImageDescription imageDescription =
@@ -137,7 +161,7 @@ public final class Examples {
     /**
      * Perform vision OCR.
      */
-    private static void visionOCR() {
+    public static void visionOCR() {
         ComputerVisionScenario computerVisionScenario = new ComputerVisionScenario(
                 getProperty("azure.cognitive.vision.subscriptionKey"));
         OCRResult ocrResult = computerVisionScenario.ocrImage(IMAGE_URL);
